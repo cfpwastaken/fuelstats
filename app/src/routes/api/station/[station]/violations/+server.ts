97a996921978e117082963d0c907bb6f278b9870
Jannik <jannik@picoscratch.de>
2026-05-15 21:45:28 +0200

    fix(station/violations): only include dates >= 2026-04-01 (#6)

--- app/src/routes/api/station/[station]/violations/+server.ts
+++ app/src/routes/api/station/[station]/violations/+server.ts
@@ -2,6 +2,6 @@ import { db } from "$lib/db.server";
 
 export async function GET({ params }: { params: { station: string } }) {
 	const { station } = params;
-	const res = await db?.query("SELECT timestamp, fuel, price, prev_price, severity, repetition_count, fee FROM illegal_changes WHERE station_uuid = $1", [station]);
+	const res = await db?.query("SELECT timestamp, fuel, price, prev_price, severity, repetition_count, fee FROM illegal_changes WHERE station_uuid = $1 AND timestamp >= '2026-04-01'", [station]);
 	return new Response(JSON.stringify(res?.rows), { status: 200 });
 }
