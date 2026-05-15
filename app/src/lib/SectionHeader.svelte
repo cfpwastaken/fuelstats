7553b22d04e1f6471278e91749d0443ba31ac1ba
Jannik <jannik@picoscratch.de>
2026-05-15 17:22:38 +0200

    feat: add anchor link to headers

--- app/src/lib/SectionHeader.svelte
+++ app/src/lib/SectionHeader.svelte
@@ -5,5 +5,6 @@
 </script>
 
 <h1 class="mb-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" {id}>
+	<a href={`#${id}`} class="text-muted-foreground hover:underline">#</a>
 	{@render children?.()}
 </h1>
