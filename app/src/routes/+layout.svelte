85f84d3770d0227c08de0d19dd48cdb4b9d20fa3
Jannik <jannik@picoscratch.de>
2026-05-15 17:53:29 +0200

    feat: embeds

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -4,9 +4,27 @@
 	import Header from '$lib/Header.svelte';
 
 	let { children } = $props();
+
+	const description = "Analyse der Kraftstoffpreise in Deutschland. Aktuelle Preise, historische Trends, illegale Preiserhöhungen und mehr. Alle Daten stammen von der Markttransparenzstelle für Kraftstoffe (MTS-K) des Bundeskartellamts.";
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /><title>Fuel Stats</title></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Fuel Stats</title>
+	<meta name="description" content={description} />
+	<meta property="og:title" content="Fuel Stats" />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content="https://cfp.is-a.dev/fuel/meme.jpg" />
+	<meta property="og:image:width" content="666" />
+	<meta property="og:image:height" content="500" />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="Fuel Stats" />
+	<meta property="og:locale" content="de_DE" />
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:title" content="Fuel Stats">
+	<meta name="twitter:description" content={description}>
+	<meta name="twitter:image" content="https://cfp.is-a.dev/fuel/meme.jpg">
+</svelte:head>
 
 <div class="max-w-dvw">
 	<!-- <Sidebar.Provider>
